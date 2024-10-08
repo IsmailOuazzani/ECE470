122800f910542a01f79692cf815793c44a37518c
Christopher Adolphe <adolphec@WIN.ecf.utoronto.ca>
2024-10-08 16:04:05 -0400

    Comment the function code

--- lab_1/forward.m
+++ lab_1/forward.m
@@ -1,4 +1,4 @@
 function [H] = forward(joint,myrobot)
-    % computes forward kinematics for the entire robot given current joint states.
+    % Computes forward kinematics for the entire robot given current joint states.
     H = forward_to_link(joint,myrobot,6);
 end
