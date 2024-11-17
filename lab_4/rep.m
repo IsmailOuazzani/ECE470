d2bd17172720f05c9370694af87ee372f363a008
ismail <ismail.ouazzani@mail.utoronto.ca>
2024-11-17 14:08:51 -0500

    fix typo in code

--- lab_4/rep.m
+++ lab_4/rep.m
@@ -16,25 +16,23 @@ function [tau] = rep(q, myrobot, obs)
         J_o_i = zeros(3,6);
         H_i = forward_to_link(q, myrobot, i);
 
-        % Computing repulsive force, assuming obstacles are either of type
+        % Computing repulsive force
         o_i = H_i(1:3,4);
         if obs.type == 'cyl' % of finite height
             d_rad = max(0, norm(o_i(1:2) - obs.c) - obs.R);
             d_z = max(0, o_i(3) - obs.h);
-            o_i_b_norm = norm(d_rad, d_z);
+            o_i_b_norm = sqrt(d_rad^2 + d_z^2);
             o_i_b = [0;0;d_z];
-            if d_rad ~= 0
-                o_i_b(1:2) = (o_i(1:2) - obs.c) * (1-obs.R/(d_rad + obs.R));
-            end
+            o_i_b(1:2) = (o_i(1:2) - obs.c) * (1-obs.R/(d_rad + obs.R));
         elseif obs.type == 'sph'
             o_i_b_norm = norm(o_i - obs.c) - obs.R;
             o_i_b = (o_i - obs.c) * (1-obs.R/(o_i_b_norm+obs.R)); 
-        else % plane
+        elseif obs.type == 'pln' % plane
             o_i_b_norm = (o_i(3) - obs.z);
             o_i_b = [0;0;o_i_b_norm];
         end
         
-        if o_i_b_norm <= obs.rho0
+        if o_i_b_norm <= obs.rho0 
             F_rep_i = eta*(1/o_i_b_norm - 1/obs.rho0)*(o_i_b/(o_i_b_norm^3));
         else
             F_rep_i = [0;0;0];
